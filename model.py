a24e75fe55018f9f1182743ae75d210ca1f402ab
J4m787 <james.yuxin.li@hotmail.com>
2022-02-25 12:46:47 +1300

    test 2

--- model.py
+++ model.py
@@ -1,10 +1,13 @@
 from msilib.schema import Class
 
+from sqlalchemy import ForeignKey, true
+from main import db
+
 
 class user(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
-    favourite_id = db.column
+    favourite_id = db.column(db.Integer, ForeignKey=True)
 
 
 
